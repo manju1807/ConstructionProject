08bb72032f940fa395d4ed117a0ffa6d0381ce4d
manju1807 <rmanjunath18@outlook.com>
2023-12-03 13:19:59 +0530

    feature update

--- src/Components/DeliverableCard.jsx
+++ src/Components/DeliverableCard.jsx
@@ -1,6 +1,6 @@
 const DeliverableCard = ({ Icon, heading, Img, Content }) => {
   return (
-    <div className="bg-white flex flex-col justify-start py-3 md:py-2 items-start max-h-fit w-full px-6 md:px-2 rounded-2xl shadow-xl border boredr-1px">
+    <div className="bg-white flex flex-col justify-start py-3 md:py-2 items-start max-h-fit w-full px-6 md:px-4 rounded-2xl shadow-xl border border-solid">
       <div className="flex flex-col space-y-2 py-2">
         <div className="bg-actionColor text-lg font-medium text-white rounded-full w-fit p-2 shadow-md">
           {Icon}
@@ -11,7 +11,7 @@ const DeliverableCard = ({ Icon, heading, Img, Content }) => {
         <div className="md:h-[200px] h-[250px] rounded-2xl">
           <img src={Img} className="h-full w-full rounded-2xl" alt="" />
         </div>
-        <p className="text-sm px-2">{Content}</p>
+        <p className="text-sm">{Content}</p>
       </div>
     </div>
   );
