32607742fa5bb5831efeb3b364f2ab075d4a3f61
manju1807 <rmanjunath18@outlook.com>
2023-12-31 20:59:19 +0530

    update

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,16 @@
+import BgComponent from '../Components/BgComponent';
+
 const Projects = () => {
-  return <div>Projects</div>;
+  const Content = () => {
+    return <div>hello</div>;
+  };
+  return (
+    <BgComponent
+      Title={'Contact Us'}
+      SubHeading={'Your Voice Matters! Reach Out – We Love to Hear from You!'}
+      Content={<Content />}
+    />
+  );
 };
 
 export default Projects;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       center: true,
     },
     fontFamily: {
-      primary: "Poppins",
-      secondary: ["inter", "sans"],
-      headingcursive: ["Allura", "serif"],
-      secondary2: ["Italianno", "cursive"],
+      primary: 'Poppins',
+      secondary: ['inter', 'sans'],
+      headingcursive: ['Allura', 'serif'],
+      secondary2: ['Italianno', 'cursive'],
     },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1440px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
     },
     extend: {
       colors: {
-        primary: "#1c292f",
-        secondary: "#d9e1e4",
-        secondary2: "#d9e1e4",
-        actionColor: "#06c39d",
-        black: "#1A1A1A",
+        primary: '#1a202c',
+        secondary: '#d9e1e4',
+        secondary2: '#d9e1e4',
+        actionColor: '#06c39d',
+        black: '#1A1A1A',
       },
     },
   },
