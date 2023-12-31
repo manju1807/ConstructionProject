2d255c7f8ac2f1e1712d1b73402d593bf4ad9149
manju1807 <rmanjunath18@outlook.com>
2023-12-31 21:36:48 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,12 @@ export default {
       secondary2: ['Italianno', 'cursive'],
     },
     screens: {
+      xs: '380px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
-      xl: '1440px',
+      xl: '1280px',
+      '2xl': '1440px',
     },
     extend: {
       colors: {
