fc0e668a0e93bc959d78914b9353701b8d333861
manju1807 <rmanjunath18@outlook.com>
2023-12-31 17:10:09 +0530

    update

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -75,12 +75,12 @@ const Contacts = () => {
                   placeholder=' '
                   required
                 />
-                <label className='peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
+                <label className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
                   Phone number
                 </label>
               </div>
-              <div className='flex flex-col gap-1'>
-                <label className='block text-sm font-medium text-gray-400'>
+              <div className='flex flex-col gap-2'>
+                <label className='block text-sm text-gray-500'>
                   Your message
                 </label>
                 <textarea
@@ -106,8 +106,8 @@ const Contacts = () => {
   };
   return (
     <BgComponent
-      Title={'About Our Company'}
-      SubHeading={'Consultation | Project Management | Construction'}
+      Title={'Contact Us'}
+      SubHeading={'Your Voice Matters! Reach Out – We Love to Hear from You!'}
       Content={<Content />}
     />
   );
